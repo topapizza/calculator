3f3cfcba783113d7e51e0379b1391b3117bf395f
toivo <toivopek@gmail.com>
2024-08-20 17:15:53 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,13 @@
-const display = document.getElementById("display");
+const display = document.getElementById("display")
 
-function appendToDisplay(input) {
-    const lastChar = display.value.slice(-1);
-
-    if (input === '.' && lastChar === '.') {
-        return;
-    }
-    
-    if (['+', '*', '/'].includes(input) && ['+', '-', '*', '/'].includes(lastChar)) {
-        return;
-    }
-
-    display.value += input;
+function appendToDisplay(input){
+    display.value += input; 
 }
 
-function clearDisplay() {
+function clearDisplay(){
     display.value = "";
 }
 
-function calculate() {
-    try {
-        display.value = eval(display.value);
-    } catch (error) {
-        display.value = "Error";
-    }
+function calculate(){
+    display.value = eval(display.value);
 }
